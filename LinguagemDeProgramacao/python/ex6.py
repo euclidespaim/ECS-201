456db28131581c656f11cdfcb4823a836e63349e
euclidespaim <euclidespaim@gmail.com>
2025-05-26 10:50:19 -0300

    Tipos de dados em Python

--- LinguagemDeProgramacao/python/ex6.py
+++ LinguagemDeProgramacao/python/ex6.py
@@ -3,3 +3,11 @@
 # e calcule a média aritmética entre eles. 
 # Mostre o resultado na tela.
 
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+media = (num1 + num2 + num3) / 3
+
+print("A média aritmética é: ", media)
+print(f"A média aritmética é: {media:.2f}")
