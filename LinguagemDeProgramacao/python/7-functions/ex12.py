b16879ff424a3bf2d15b73fb87de78c356c9d4b8
euclidespaim <euclidespaim@gmail.com>
2025-07-10 12:57:04 -0300

    functions em python

--- LinguagemDeProgramacao/python/7-functions/ex12.py
+++ LinguagemDeProgramacao/python/7-functions/ex12.py
@@ -1,6 +1,10 @@
 # Crie uma função quadrado que:
-
 # Receba um número como parâmetro
-
 # Retorne o quadrado desse número
 
+def quadrado(numero):
+    return numero ** 2
+  
+
+num = int(input("Digite um número: "))
+print("O quadrado de", num, "é", quadrado(num), ".")
