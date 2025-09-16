7ca38f1532cacac003ff2dcf96d830f896e68ce7
euclidespaim <euclidespaim@gmail.com>
2025-09-16 14:02:29 -0300

    Exemplos JS!

---
+++ ProgramacaoWeb/exemplos/10-javascript/site-1/script.js
@@ -0,0 +1,3 @@
+document.getElementById("soma").innerText = "2 + 3 = " + (2 + 3);
+
+window.alert("Olá, mundo!");
---
+++ ProgramacaoWeb/exemplos/10-javascript/site-2/script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").onclick = function(){
+  
+  document.body.style.backgroundColor = "lightblue";
+  document.getElementById("status").innerText = "Evento onClick disparado!"
+}
